79b761a7b675786593ff1324a44258263f48ac27
azzamallow <azzamallow@gmail.com>
2014-03-28 16:02:10 +1100

    layouts on phone

--- app/scripts/controllers/floks.js
+++ app/scripts/controllers/floks.js
@@ -5,7 +5,7 @@ angular.module('assembleApp')
 
     $scope.people = [
       { name: 'Cam Hine', distance: '2 kilometres' },
-      { name: 'Aaron Triantafylidis', distance: '2 kilometres' },
+      { name: 'Brendan Spinks', distance: '2 kilometres' },
       { name: 'Nuzakat Ali', distance: '1.4 kilometres' },
       { name: 'Amir Ansari', distance: '200 metres' },
       { name: 'Marc Phoa', distance: '800 metres' }
